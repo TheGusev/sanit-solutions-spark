54b0c85ad34c1e768bf974755728ea868b5842bb
TheGusev <dpd.tuva@mail.ru>
2026-01-26 20:09:37 +0300

    Fix syntax error in vite.config.ts - add missing ]

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig(({ mode }) => ({
 
         // 🚀 Gzip compression
     
-    filter(Boolean),
+    filter(Boolean)],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
